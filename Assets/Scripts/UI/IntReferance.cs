77d9aeaf6f4b2253846dda0a322f7d0f222d347c
MatteoPetronelli <matteo.petronelli5@gmail.com>
2025-05-06 23:04:27 +0200

    GameParameter modif

--- Assets/Scripts/UI/IntReferance.cs
+++ Assets/Scripts/UI/IntReferance.cs
@@ -1,126 +1,56 @@
 using UnityEngine;
-using System.Reflection;
+using UnityEngine.UI;
 using TMPro;
-using System;
+
+public enum Values
+{
+    NeedToWin,
+    PlayerHealth
+}
 
 [System.Serializable]
 public class IntReference
 {
-    private GameManager gameManager => GameManager.Instance;
-    public TextMeshProUGUI textObject;
-    public MonoBehaviour target;
-    public string fieldName;
-    public float minValue;
-    public float maxValue;
-    public float defaultValue;
-    public bool isInfinite;
+    public TMP_Text textField;
+    public Button addValueButton;
+    public Button removeValueButton;
+    public Values valueType;
+
+    private int CurrentValue
+    {
+        get
+        {
+            return valueType switch
+            {
+                Values.NeedToWin => GameParameter.Instance.GetNeedToWin(),
+                Values.PlayerHealth => GameParameter.Instance.GetPlayerHealth(),
+                _ => 0
+            };
+        }
+        set
+        {
+            switch (valueType)
+            {
+                case Values.NeedToWin:
+                    GameParameter.Instance.SetNeedToWin(value);
+                    break;
+                case Values.PlayerHealth:
+                    GameParameter.Instance.SetPlayerHeal(value);
+                    break;
+            }
+            UpdateText();
+        }
+    }
 
     public void Initialize()
     {
-        SetValue(defaultValue);
+        addValueButton.onClick.AddListener(AddValue);
+        removeValueButton.onClick.AddListener(RemoveValue);
+        UpdateText();
     }
 
-    public float GetValue()
-    {
-        if (target == null || string.IsNullOrEmpty(fieldName))
-            return 0;
-
-        var type = target.GetType();
-        var field = type.GetField(fieldName,
-            BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-
-        if (target.name == "Player")
-        {
-            var players = gameManager.GetAllPlayers();
-            if (players.Length == 0) return int.Parse(textObject.text);
-            var playerField = players[0].GetType().GetField(fieldName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-            if (playerField.FieldType == typeof(int))
-            {
-                return (int)playerField.GetValue(players[0]);
-            }
-            else if (playerField.FieldType == typeof(float))
-            {
-                return (int)(float)playerField.GetValue(players[0]);
-            }
-        }
-        if (field != null && field.FieldType == typeof(int))
-        {
-            return (int)field.GetValue(target);
-        } else if (field != null && field.FieldType == typeof(float))
-        {
-            return (int)(float)field.GetValue(target);
-        }
-
-        Debug.LogWarning($"Field '{fieldName}' not found or not int in '{target.name}'");
-        return 0;
-    }
-
-    public void SetValue(float value)
-    {
-        if (target == null || string.IsNullOrEmpty(fieldName))
-            return;
-
-        var type = target.GetType();
-        var field = type.GetField(fieldName,
-            BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-
-        if (field != null && (field.FieldType == typeof(int) || field.FieldType == typeof(float)))
-        {
-            if (target.name == "Player")
-            {
-                var players = gameManager.GetAllPlayers();
-                if (players.Length == 0) textObject.text = value.ToString();
-                foreach (var player in players)
-                {
-                    var playerField = player.GetType().GetField(fieldName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-                    if (field.FieldType == typeof(int)){
-                        playerField.SetValue(player, (int)value);
-                    }
-                    else if (field.FieldType == typeof(float)){
-                        playerField.SetValue(player, value);
-                    }
-                }
-
-            }
-            else if (field.FieldType == typeof(int)){
-                field.SetValue(target, (int)value);
-            }
-            else if (field.FieldType == typeof(float)){
-                field.SetValue(target, value);
-            }
-            
-            if (value == 10000)
-            {
-                textObject.text = "∞";
-                return;
-            }
-            textObject.text = value.ToString();
-        }
-        else
-        {
-            Debug.LogWarning($"Field '{fieldName}' not found or not int in '{target.name}'");
-        }
-    }
-
-    public void Increment()
-    {
-        float newValue = GetValue() + 1;
-        if (isInfinite && newValue > maxValue)
-        {
-            SetValue(10000);
-            return;
-        }
-        SetValue(newValue > maxValue ? minValue : newValue);
-    }
-
-    public void Decrement()
-    {
-        if (GetValue() == 10000)
-        {
-            SetValue(maxValue);
-            return;
-        }
-        float newValue = GetValue() - 1;
-        SetValue(newValue < minValue ? maxValue : newValue);
-    }
+    private void AddValue() => CurrentValue++;
+    private void RemoveValue() => CurrentValue = Mathf.Max(0, CurrentValue - 1);
+    private void UpdateText() => textField.text = CurrentValue.ToString();
 }
+
